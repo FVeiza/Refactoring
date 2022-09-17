6bc79143872f327e27110fd17028bc44e3c285d1
FVeiza <fegmve@gmail.com>
2022-09-17 20:02:17 -0300

    Commit 9

--- Main/Rental.java
+++ Main/Rental.java
@@ -18,32 +18,13 @@ public class Rental {
     }
 
     public double getCharge() {
-        
-        double thisAmount = 0;
-        //determine amounts for each line
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (this.getDaysRented() > 2)
-                    thisAmount += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (this.getDaysRented() > 3)
-                    thisAmount += (this.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        
-        return thisAmount;
-   }
+        return _movie.getCharge(_daysRented);
+    }
 
-   public int getFrequentRenterPoints(){
-       // add bonus for a two day new release rental
+    public int getFrequentRenterPoints(){
+        // add bonus for a two day new release rental
         if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
             this.getDaysRented() > 1) return 2;
         return 1;
-   }
+    }
 }
